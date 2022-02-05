b8b044007a007cf7729dc05be63dd8c6df0d453c
Mihir Deshpande <74093676+mihirmsdp@users.noreply.github.com>
2022-02-05 14:00:21 +0530

    Create starpattern.cpp

---
+++ Patterns2/starpattern.cpp
@@ -0,0 +1,64 @@
+Print the following pattern
+Pattern for N = 4
+
+
+
+The dots represent spaces.
+
+
+
+Input Format :
+N (Total no. of rows)
+Output Format :
+Pattern in N lines
+Constraints :
+0 <= N <= 50
+Sample Input 1 :
+3
+Sample Output 1 :
+   *
+  *** 
+ *****
+Sample Input 2 :
+4
+Sample Output 2 :
+    *
+   *** 
+  *****
+ *******
+ 
+      /*code*/
+      
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    int n;
+    cin>>n;
+    int i =1;
+    while(i<=n){
+        int spaces=1;
+        while(spaces<=n-i){
+            cout<<" ";
+            spaces++;
+        }
+        int stars = 1;
+        while(stars<=i){
+            cout<<"*";
+            stars++;
+        }
+        stars = i -1;
+        while(stars>=1){
+            cout<<"*";
+            stars--;
+        }
+        cout<<endl;
+        i ++;
+    }
+
+       /*  Read input as specified in the question.
+	* Print output as specified in the question.
+	*/
+  
+}
